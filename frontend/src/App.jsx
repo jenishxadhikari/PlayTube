7759ecdc96998b07f9fe86ad2ddeea0d0c6c23c9
jenishxadhikari <114239379+jenishxadhikari@users.noreply.github.com>
2025-02-07 00:43:06 +0545

    chore(frontend): initial setup
    - initialize vite + react
    - add formatting & linting with pre-commit hooks
    - add tailwindcss & shadcn

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+
+export default function App() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-5">
+      <h1 className="text-4xl font-bold tracking-tight">PlayTube</h1>
+      <Button>Hello World</Button>
+    </main>
+  )
+}
